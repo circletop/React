d885897972b860c13ead240a5a074c732c999f29
冷兴达 <648612680@qq.com>
2023-11-15 14:55:34 +0800

    初始化设置

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// 样式重置
+import "rest-css"
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
