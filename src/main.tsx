1e116828fca3c0fb3a93a980982c662f89aee5c4
冷兴达 <648612680@qq.com>
2023-11-15 15:48:45 +0800

    配置路径别名

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 // 样式重置
 import "rest-css"
 // 全局样式
-import "./assets/styles/global.scss"
+import "@/assets/styles/global.scss"
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
