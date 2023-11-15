7e741780228219a5a9354655c9f47d2c789764bc
冷兴达 <648612680@qq.com>
2023-11-15 15:04:54 +0800

    sass安装和全局样式设置

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // 样式重置
 import "rest-css"
+// 全局样式
+import "./assets/styles/global.scss"
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
