82237d23b6b0b61410407b4d9db937b9f704e3d2
冷兴达 <648612680@qq.com>
2023-11-23 09:54:09 +0800

    登陆页面书写

---
+++ src/store/reducer.ts
@@ -0,0 +1,16 @@
+const nameInitialState = {
+  a: 1
+}
+const reducer = (state = nameInitialState, action) => {
+  // switch (action.type) {
+  //   case ACTION_TYPE_1:
+  //     return state
+  //   case ACTION_TYPE_2:
+  //     return state
+  //   default:
+  //     return state
+  // }
+  return state
+}
+
+export default reducer
