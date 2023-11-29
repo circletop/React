419af15fd44d36e634b7eee4616c48c0cbe80e0b
冷兴达 <648612680@qq.com>
2023-11-29 15:20:21 +0800

    路由守卫

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import { useRoutes } from 'react-router-dom';
+import { useLocation, useRoutes, useNavigate } from 'react-router-dom';
 import router from './router';
+import { useEffect } from 'react';
+
+
+const ToPage=()=> {
+  const navTo = useNavigate()
+  useEffect(()=> {
+    navTo('/page1')
+  }, [navTo])
+  return <div></div>
+}
+const ToLogin = ()=> {
+  const navTo = useNavigate()
+  useEffect(()=> {
+    navTo('/login')
+  }, [navTo])
+  return <div></div>
+}
+// 路由守卫
+const BeforeRouterEnter = ()=> {
+  const token = sessionStorage.getItem('token')
+  const outlet = useRoutes(router)
+  const location = useLocation();
+
+  if (token && location.pathname === '/login' ) {
+    return <ToPage />
+  }
+  if(!token && location.pathname !=='/login') {
+    return <ToLogin />
+  }
+  return outlet
+}
 
 function App() {
-const outlet = useRoutes(router )
+// const outlet = useRoutes(router )
   return (
   <>
-      {outlet}
+      {BeforeRouterEnter()}
   </>
   )
 }
